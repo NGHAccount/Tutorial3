d8a27267462a39da8fa3c672301378ae7b55c23a
Varitek3rulz3 <nickhennessey3@gmail.com>
2019-11-05 14:19:15 -0500

    Push

---
+++ Tutorial_3/Assets/Final/Scripts/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Done_RandomRotator : MonoBehaviour 
+{
+    private Rigidbody rb;
+
+    public float tumble;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
